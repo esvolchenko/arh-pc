8da9e3da61624ab052c9a9d8c751d40cd125b28b
esvolchenko <ellen.volchenko@gmail.com>
2022-11-17 17:58:02 +0300

    feat(main): make course structure

---
+++ labs/lab05/lab05.asm
@@ -0,0 +1,20 @@
+; lab05.asm
+SECTION .data 				; Начало секции данных
+	lab05: DB 'Elena Volchenko',10 	; 'Elena Volchenko' плюс
+					; символ перевода строки
+					
+lab05Len: EQU $-lab05 			; Длина строки lab05
+
+SECTION .text 			; Начало секции кода
+GLOBAL _start
+
+_start: 			; Точка входа в программу
+	mov eax,4 		; Системный вызов для записи (sys_write)
+	mov ebx,1 		; Описатель файла '1' - стандартный вывод
+	mov ecx,lab05 		; Адрес строки lab05 в ecx
+	mov edx,lab05Len 	; Размер строки lab05
+	int 80h 		; Вызов ядра
+	
+	mov eax,1 		; Системный вызов для выхода (sys_exit)
+	mov ebx,0 		; Выход с кодом возврата '0' (без ошибок)
+	int 80h 		; Вызов ядра
